4aebe3e39892ac1cc650ff7bb085cfe6f9be34a0
d-kiss <speakupness@gmail.com>
2019-02-22 16:33:25 +0200

    Added tests for MaxHeap as well.

--- tests/tests.py
+++ tests/tests.py
@@ -17,3 +17,35 @@ def test_min_heap():
     assert heap.extract() == 2
     assert heap.extract() == 3
     assert heap.extract() == 4
+
+    heap = MinHeap.build_from_array([1, 4, 2, 3])
+
+    assert heap.extract() == 1
+    assert heap.extract() == 2
+    assert heap.extract() == 3
+    assert heap.extract() == 4
+
+
+def test_max_heap():
+    heap = MaxHeap()
+    heap.insert(2)
+    heap.insert(1)
+    heap.insert(3)
+
+    assert heap.top() == 3
+
+    heap.insert(4)
+
+    assert heap.top() == 4
+
+    assert heap.extract() == 4
+    assert heap.extract() == 3
+    assert heap.extract() == 2
+    assert heap.extract() == 1
+
+    heap = MaxHeap.build_from_array([1, 4, 2, 3])
+
+    assert heap.extract() == 4
+    assert heap.extract() == 3
+    assert heap.extract() == 2
+    assert heap.extract() == 1
